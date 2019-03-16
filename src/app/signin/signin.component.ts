f3fd0473507919055c82da8d2219a78c54966953
e0eee <dmitriy_girskiy@yahoo.com>
2019-03-16 16:03:15 +0300

    connect signin service

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PassportService } from '../passport.service';
+import { CookieService } from '../cookie.service';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  private token;
+  public data = {
+    "email":"",
+    "password":""
+  };
+  
+  constructor(
+    private passportService: PassportService,
+    private cookieService: CookieService,
+    private router: Router) { }
 
   ngOnInit() {
+    if (this.cookieService.getCookie('login')) {
+      this.router.navigate(['/']);
+    };
   }
 
+  onSubmit() {
+    this.passportService.auth(this.data).subscribe(
+      user => {
+        this.cookieService.setCookie('login',(user['success'].token), {});
+        this.router.navigate(['/']);
+      }
+    );
+  }
 }
