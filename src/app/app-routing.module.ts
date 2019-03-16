0dabd79455d2decee79a4860ac0a192be03bebc0
e0eee <dmitriy_girskiy@yahoo.com>
2019-03-16 15:12:42 +0300

    setup forget-password component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { ForgetComponent } from './forget/forget.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/signin', pathMatch: 'full' },
   { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'forget-password', component: ForgetComponent }
 ];
 
 @NgModule({
