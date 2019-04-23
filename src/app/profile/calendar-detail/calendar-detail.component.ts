4c3b909371f51ec83461c44b5352c0c680731d2d
kuznetsovsky <kuznetsovsky@inbox.ru>
2019-04-23 15:00:24 +0400

    Меняет расширение компонента sass на scss

--- src/app/profile/calendar-detail/calendar-detail.component.ts
+++ src/app/profile/calendar-detail/calendar-detail.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calendar-detail',
   templateUrl: './calendar-detail.component.html',
-  styleUrls: ['./calendar-detail.component.sass']
+  styleUrls: ['./calendar-detail.component.scss']
 })
 export class CalendarDetailComponent implements OnInit {
 
