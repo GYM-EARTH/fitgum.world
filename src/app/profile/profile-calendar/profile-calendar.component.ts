c9ea47fd5c35bb88b23f098b1cfdb641e2d69a49
e0eee <dmitriy_girskiy@yahoo.com>
2019-05-04 12:19:18 +0300

    url states

--- src/app/profile/profile-calendar/profile-calendar.component.ts
+++ src/app/profile/profile-calendar/profile-calendar.component.ts
@@ -9,10 +9,19 @@ import { CookieService } from 'src/app/cookie.service';
 })
 export class ProfileCalendarComponent implements OnInit {
 
+  public Date = new Date();
+  // year = this.Date.getYear();
+  month = this.Date.getMonth();
+  days = this.Date.getDay();
+  
+  nameDay = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+  nameMonth = ['Декабрь', 'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь'];
+
   constructor(private router: Router, private cookieService: CookieService,) {
     if (!this.cookieService.getCookie('login')) {
       this.router.navigate(['/signin']);
     };
+    console.log(this.month);
   }
   ngOnInit() {
   }
