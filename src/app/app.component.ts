ae1b7167b30fb50cfcb5bf70719361ce75cdd112
e0eee <dmitriy_girskiy@yahoo.com>
2019-05-04 16:06:43 +0300

    theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fitgum';
+  
+  constructor() {
+    const hours = new Date().getHours()
+    const isDayTime = hours >= 6 && hours <= 18;
+
+    if (isDayTime) {
+      document.body.classList.add('white');
+    } else {
+      document.body.classList.add('black');
+    }
+  }
 }
