fbbe879810a5cf29a47e2543d1a7fdb4b4aed5d8
e0eee <dmitriy_girskiy@yahoo.com>
2019-05-04 18:11:49 +0300

    welcome

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from '../cookie.service';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  public auth: Boolean = false;
+
+  constructor(private cookieService: CookieService) { }
 
   ngOnInit() {
+    if (this.cookieService.getCookie('login')) {
+      this.auth = true;
+    }
   }
 
 }
