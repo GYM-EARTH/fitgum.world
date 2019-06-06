103c132fc5627822e9b81322475948e8443a4e6f
e0eee <dmitriy_girskiy@yahoo.com>
2019-06-06 21:23:21 +0300

    update videos

--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { VideosService } from '../videos.service';
+import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-videos',
@@ -9,9 +10,10 @@ import { VideosService } from '../videos.service';
 export class VideosComponent implements OnInit {
 
   public videos;
-  firstVideo = null;
+  public firstVideo;
+  url: SafeResourceUrl;
   
-  constructor(private videosService: VideosService) { }
+  constructor(private videosService: VideosService, public sanitizer:DomSanitizer) { }
 
   ngOnInit() {
     this.videosService.getAll().subscribe(videos => {
@@ -21,7 +23,8 @@ export class VideosComponent implements OnInit {
   }
 
   selectVideo(target) {
-    this.firstVideo = target;
+    this.firstVideo = 'https://www.youtube-nocookie.com/embed/' + target + '?controls=1&autoplay=1&mute=1&showinfo=0&loop=1&playlist=t2D0NOKVWXo';
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.firstVideo);  
   }
 
 }
