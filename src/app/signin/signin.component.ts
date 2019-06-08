ca66c91cb0655bddc7603d377992352067005d80
e0eee <dmitriy_girskiy@yahoo.com>
2019-06-08 12:54:42 +0300

    set meta tags sigin signup

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PassportService } from '../passport.service';
 import { CookieService } from '../cookie.service';
 import { Router } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent implements OnInit {
+export class SigninComponent implements OnInit, OnDestroy {
 
   private token;
   public data = {
@@ -16,15 +17,31 @@ export class SigninComponent implements OnInit {
     "password":""
   };
   
+  private title;
+
   constructor(
     private passportService: PassportService,
     private cookieService: CookieService,
-    private router: Router) { }
+    private router: Router,
+    private meta: Meta,
+    private titleService: Title) {
+      this.title = this.titleService.getTitle();
+    }
 
   ngOnInit() {
+
+    this.titleService.setTitle('Войти'),
+    this.meta.updateTag({ name: 'description', content: 'Откройте Ваш мир по фитнесу' });
+    this.meta.updateTag({property: 'og:title', content: 'Войти'});
+    this.meta.updateTag({property: 'og:description', content: 'Откройте Ваш мир по фитнесу'});
+
     if (this.cookieService.getCookie('login')) {
       this.router.navigate(['/']);
     };
+  }
+
+  ngOnDestroy() {
+    this.titleService.setTitle(this.title);
   }
 
   onSubmit() {
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { PassportService } from '../passport.service';
 import { CookieService } from '../cookie.service';
 import { Router } from '@angular/router';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
+
+  private title;
 
   constructor(
     private passportService: PassportService,
     private cookieService: CookieService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private meta: Meta,
+    private titleService: Title
+  ) {
+    this.title = this.titleService.getTitle();
+  }
 
   public data = {
     "name":"",
@@ -23,9 +30,20 @@ export class SignupComponent implements OnInit {
   };
 
   ngOnInit() {
+
+    this.titleService.setTitle('Регистрация'),
+    this.meta.updateTag({ name: 'description', content: 'Откройте Ваш мир по фитнесу' });
+    this.meta.updateTag({property: 'og:title', content: 'Регистрация'});
+    this.meta.updateTag({property: 'og:description', content: 'Откройте Ваш мир по фитнесу'});
+
     if (this.cookieService.getCookie('login')) {
       this.router.navigate(['/']);
     };
+  }
+
+
+  ngOnDestroy() {
+    this.titleService.setTitle(this.title);
   }
 
   onSubmit() {
